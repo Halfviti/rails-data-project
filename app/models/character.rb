f2f9a25db1741ea0e273403a913ac05bcf0c046d
Gavin Johnson <g.ks.johnson@gmail.com>
2018-10-10 09:42:06 -0500

    Added validations to models.

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,6 @@
 class Character < ApplicationRecord
+  belongs_to :homeworld, :class_name => 'Planet', :foreign_key => 'planet_id'
+  has_many :films, through :appearances
+
+  validates :name, presence: true
 end
--- app/models/planet.rb
+++ app/models/planet.rb
@@ -1,2 +1,3 @@
 class Planet < ApplicationRecord
+  validates :name, presence: true, uniqueness: true
 end
