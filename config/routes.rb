004259c880dfc9c815a3e0930c0012910b9070aa
Gavin Johnson <g.ks.johnson@gmail.com>
2018-10-11 19:42:54 -0500

    Added character controller/views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'planets/index'
+  get 'planets/show'
+  resources :characters, only: [:index, :show]
+  resources :welcome, only: [:index]
 
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
